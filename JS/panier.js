54cac29111af2978b0520cda575124add5234ab1
YoshiOnly <jonathan.hingue.buchanan@gmail.com>
2021-07-28 18:42:48 +0200

    modification du regex

--- JS/panier.js
+++ JS/panier.js
@@ -28,7 +28,7 @@ let commandForm = { contact, products};
 const regexName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
 const regexCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
 const regexMail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-zA-Z]{2,4}$/;
-const regexAddress = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
+const regexAddress = /^(([a-zA-ZÀ-ÿ0-9]))/;
 
 main()
 
